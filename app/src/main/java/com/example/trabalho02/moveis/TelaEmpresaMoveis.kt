c909b3ad7fb420c94dfefa8ba084ed7d3c11b668
OPaje <jecssilva@gmail.com>
2023-04-11 17:27:44 -0300

    Mostrando opções do crud, tela inserir criada

--- app/src/main/java/com/example/trabalho02/moveis/TelaEmpresaMoveis.kt
+++ app/src/main/java/com/example/trabalho02/moveis/TelaEmpresaMoveis.kt
@@ -1,19 +1,43 @@
 package com.example.trabalho02.moveis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.trabalho02.R
 import com.example.trabalho02.databinding.TelaEmpresaMoveisBinding
 
 class TelaEmpresaMoveis : AppCompatActivity() {
 
-    lateinit var binding : TelaEmpresaMoveisBinding
-
+    lateinit var binding: TelaEmpresaMoveisBinding
+    val listaMoveis: ArrayList<Movel> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = TelaEmpresaMoveisBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val opcoesCrud = resources.getStringArray(R.array.opcoes_crud_movel)
+        val adaptador =
+            ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, opcoesCrud)
+        binding.lvOpcoesCrudMovel.adapter = adaptador
 
+        val register = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result: ActivityResult ->
+            if (result.resultCode == RESULT_OK) {
+                result.data?.let {
+                    if (it.hasExtra("555")) {
+                        val movel: Movel = it.getParcelableExtra("555")!!
+                        listaMoveis.add(movel)
+                    }
+                }
+            }
+        }
 
+        val opcoes = hashMapOf(
+            "Inserir Móveis" to {register.launch(Intent(applicationContext, TelaInserirMovel::class.java))}
+        )
     }
 }
---
+++ app/src/main/java/com/example/trabalho02/moveis/TelaInserirMovel.kt
@@ -0,0 +1,13 @@
+package com.example.trabalho02.moveis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trabalho02.R
+
+
+class TelaInserirMovel : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela_inserir_movel)
+    }
+}
